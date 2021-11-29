c59e44a793bbfd5821ed063802ca3179b8ee6d8f
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2021-11-28 20:41:12 -0500

    ajustes proyecto serializer

--- ms2App/serializers/proyectoSerializer.py
+++ ms2App/serializers/proyectoSerializer.py
@@ -1,7 +1,26 @@
-from ms2App.models.proyecto import Proyectos
+from ms2App.models.proyecto import Proyectos, Tecnologia, Duracion, Enlaces
 from rest_framework import serializers
+
+
+class TecnologiaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tecnologia
+        fields = '__all__'
+
+
+class DuracionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Duracion
+        fields = '__all__'
+
+
+class EnlacesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enlaces
+        fields = '__all__'
+
 
 class ProyectoSerializer(serializers.ModelSerializer):
     class Meta:
-       model = Proyectos
-       fields = '__all__'
+        model = Proyectos
+        fields = '__all__'
